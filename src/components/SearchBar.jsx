3e31b9b48722b67a2a8810d062b7de24616a73d1
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-29 20:27:00 +0100

    layout done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import searchIcon from "../assets/icon-search.svg";
 
 export default function SearchBar() {
-  return <h1>Search Bar</h1>;
+  return (
+    <div className="searchBar">
+      <img src={searchIcon} alt="search icon"></img>
+      <input placeholder="Search for movies or TVs"></input>
+      <button>search</button>
+    </div>
+  );
 }
