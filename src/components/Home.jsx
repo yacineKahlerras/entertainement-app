1f0bec772d6e475ef12a537755f2c327ca3980de
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-30 12:15:11 +0100

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { data } from "../data";
+import HomeSection from "./home/HomeSection";
 import Trending from "./home/Trending";
 
 export default function Home() {
   return (
     <div className="home">
       <Trending />
+      <HomeSection
+        headerTitle="Popular"
+        category="movies"
+        list={data.results.slice(0, 8)}
+      />
     </div>
   );
 }
--- src/components/home/HomeSection.jsx
+++ src/components/home/HomeSection.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import HomeSectionSlide from "./HomeSectionSlide";
 
 export default function HomeSection(props) {
-  const { headerTitle, category } = props;
+  const { headerTitle, category, list } = props;
+
+  function slides() {
+    const newList = list.map((element, index) => {
+      const { backdrop_path, title, release_date, movie } = element;
+      return (
+        <HomeSectionSlide
+          key={index}
+          src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+          title={title}
+          genre={movie === false ? "movie" : "TV Series"}
+          year={release_date}
+        />
+      );
+    });
+    return newList;
+  }
+
   return (
     <div className="home-section">
       <header className="section-header">
@@ -9,6 +27,7 @@ export default function HomeSection(props) {
         <span className="header-category">{category}</span>
         <button>see more</button>
       </header>
+      <div className="display-map">{slides()}</div>
     </div>
   );
 }
