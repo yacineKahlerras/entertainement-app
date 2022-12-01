06227cb665edfb4b47debd0f58e1158554cae4cd
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-02 00:24:16 +0100

    ok

--- src/components/home/HomeSectionSlide.jsx
+++ src/components/home/HomeSectionSlide.jsx
@@ -12,6 +12,7 @@ export default function HomeSectionSlide(props) {
       first_air_date,
       video,
       backdrop_path,
+      poster_path,
     },
   } = props;
 
@@ -19,18 +20,18 @@ export default function HomeSectionSlide(props) {
   const year = video === false ? release_date : first_air_date;
   const genre = video === false ? "movie" : "TV series";
   const showSlide = backdrop_path !== null;
+  const imgSrc = backdrop_path !== null ? backdrop_path : poster_path;
 
   return (
     <Link to={`/${video === false ? "movie" : "tv-show"}/${id}`}>
-      <div
-        className="home-section-slide"
-        style={{ display: showSlide ? "block" : "none" }}
-      >
-        <img
-          className="slide-cover"
-          src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
-          alt={title}
-        ></img>
+      <div className="home-section-slide">
+        <div className="slide-cover-container">
+          <img
+            className="slide-cover"
+            src={`https://image.tmdb.org/t/p/w500${imgSrc}`}
+            alt={title}
+          ></img>
+        </div>
         <div className="home-section-slide-text">
           <span className="info">
             {String(year).substring(0, 4)}{" "}
